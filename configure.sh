a4e5fd6674f71be655fded3264c74df0cbd698ae
Krzesimir Nowak <qdlacz@gmail.com>
2015-09-12 15:14:40 +0200

    initial commit

---
+++ configure.sh
@@ -0,0 +1,10 @@
+git config --global user.name "Krzesimir Nowak"
+git config --global user.email qdlacz@gmail.com
+git config --global core.editor emacs-nox
+git config --global push.default simple
+git config --global sendemail.from "Krzesimir Nowak <qdlacz@gmail.com>"
+git config --global sendemail.smtpserver smtp.gmail.com
+git config --global sendemail.smtpuser qdlacz@gmail.com
+git config --global sendemail.smtpencryption tls
+git config --global sendemail.smtpserverport 587
+git config --global credential.helper gnome-keyring
