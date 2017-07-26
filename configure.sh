5389a7a049351eac98816e187fc0dfd296f265ba
Krzesimir Nowak <qdlacz@gmail.com>
2017-07-26 12:47:31 +0200

    allow different email

--- configure.sh
+++ configure.sh
@@ -1,10 +1,18 @@
+#!/usr/bin/bash
+
+email="${1}"
+if [[ -z "${email}" ]]; then
+    email='qdlacz@gmail.com'
+    echo "No email given, defaulting to ${email}"
+fi
+
 git config --global user.name "Krzesimir Nowak"
-git config --global user.email qdlacz@gmail.com
+git config --global user.email "${email}"
 git config --global core.editor emacs-nox
 git config --global push.default simple
-git config --global sendemail.from "Krzesimir Nowak <qdlacz@gmail.com>"
+git config --global sendemail.from "Krzesimir Nowak <${email}>"
 git config --global sendemail.smtpserver smtp.gmail.com
-git config --global sendemail.smtpuser qdlacz@gmail.com
+git config --global sendemail.smtpuser "${email}"
 git config --global sendemail.smtpencryption tls
 git config --global sendemail.smtpserverport 587
 git config --global credential.helper gnome-keyring
