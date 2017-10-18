2924046415e86065e53fe2ee996034a5209a85e9
Krzesimir Nowak <qdlacz@gmail.com>
2017-10-18 12:44:57 +0200

    prefer libsecret credential provider over gnome-keyring one
    The latter is deprecated and may not exist anymore.

--- configure.sh
+++ configure.sh
@@ -15,5 +15,6 @@ git config --global sendemail.smtpserver smtp.gmail.com
 git config --global sendemail.smtpuser "${email}"
 git config --global sendemail.smtpencryption tls
 git config --global sendemail.smtpserverport 587
-git config --global credential.helper gnome-keyring
+git config --global credential.helper libsecret
+git config --global --add credential.helper gnome-keyring
 git config --global rebase.autosquash true
