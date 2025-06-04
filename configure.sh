4a80f056e782623bef049389ac2762021f1f8257
Krzesimir Nowak <qdlacz@gmail.com>
2025-06-04 11:00:16 +0200

    sort, add safe directory

--- configure.sh
+++ configure.sh
@@ -6,15 +6,15 @@ if [[ -z "${email}" ]]; then
     echo "No email given, defaulting to ${email}"
 fi
 
-git config --global core.editor emacs -nw
-git config --global credential.helper libsecret
-git config --global --add credential.helper gnome-keyring
-git config --global push.default simple
-git config --global rebase.autosquash true
-git config --global sendemail.from "Krzesimir Nowak <${email}>"
-git config --global sendemail.smtpserver smtp.gmail.com
-git config --global sendemail.smtpuser "${email}"
-git config --global sendemail.smtpencryption tls
-git config --global sendemail.smtpserverport 587
 git config --global user.name "Krzesimir Nowak"
 git config --global user.email "${email}"
+git config --global core.editor "emacs -nw"
+git config --global push.default simple
+git config --global credential.helper libsecret
+git config --global rebase.autosquash true
+git config --global sendemail.smtpserver smtp.gmail.com
+git config --global sendemail.smtpserverport 587
+git config --global sendemail.smtpencryption tls
+git config --global sendemail.smtpuser "${email}"
+git config --global sendemail.from "Krzesimir Nowak <${email}>"
+git config --global safe.directory '*'
